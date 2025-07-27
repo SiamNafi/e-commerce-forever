c9ceaac43e7d1cfa74f2c55a68389762b22a66cd
Nafiuls <snafiul700@gmail.com>
2025-07-28 00:53:01 +0600

    collection page done alhamdulillah

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import { assets } from "../assets/assets";
 import { Link, NavLink } from "react-router-dom";
+import UseShop from "../utils/UseShop";
 const Navbar = () => {
   const [visible, setVisible] = useState(false);
+  const { setShowSearch } = UseShop();
   return (
     <div className="flex items-center justify-between py-5 font-medium">
       {/* logo */}
@@ -32,7 +34,12 @@ const Navbar = () => {
         </NavLink>
       </ul>
       <div className="flex items-center gap-6">
-        <img src={assets.search_icon} className="w-5 cursor-pointer" alt="" />
+        <img
+          onClick={() => setShowSearch(true)}
+          src={assets.search_icon}
+          className="w-5 cursor-pointer"
+          alt=""
+        />
         <div className="group relative">
           <img
             src={assets.profile_icon}
--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,14 +1,20 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { products } from "../assets/assets";
 export const ShopContext = createContext();
 
 export const ShopContextProver = ({ children }) => {
   const currency = "$";
   const delivary_fee = 10;
+  const [search, setSearch] = useState("");
+  const [showSearch, setShowSearch] = useState(false);
   const value = {
     products,
     currency,
     delivary_fee,
+    search,
+    setSearch,
+    showSearch,
+    setShowSearch,
   };
   return <ShopContext.Provider value={value}>{children}</ShopContext.Provider>;
 };
