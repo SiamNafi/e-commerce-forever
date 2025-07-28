592a43880c74a8374473504c818671130d1fcc0b
Nafiuls <snafiul700@gmail.com>
2025-07-29 01:02:34 +0600

    place order page done Alhamdulillah

---
+++ frontend/src/components/CartTotal.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import UseShop from "../utils/UseShop";
+import Title from "./Title";
+
+const CartTotal = () => {
+  const { getCartAmount, currency, delivary_fee } = UseShop();
+  return (
+    <div className="w-full">
+      <div className="text-2xl">
+        <Title text1={"CART"} text2={"TOTALS"} />
+      </div>
+      <div className="flex flex-col gap-2 mt-2 text-sm">
+        <div className="flex justify-between">
+          <p>Subtotal</p>
+          <p>
+            {currency} {getCartAmount()}.00
+          </p>
+        </div>
+        <hr />
+        <div className="flex justify-between">
+          <p>Shipping Fee</p>
+          <p>
+            {currency} {delivary_fee}.00
+          </p>
+        </div>
+        <hr />
+        <div className="flex justify-between">
+          <b>Total</b>
+          <b>
+            {currency}{" "}
+            {getCartAmount() === 0 ? 0 : getCartAmount() + delivary_fee}.00
+          </b>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartTotal;
