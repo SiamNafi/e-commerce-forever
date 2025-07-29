5ed3be6aa03a86f5c960b70aaf0ac36004ec4fb5
Nafiuls <snafiul700@gmail.com>
2025-07-29 13:07:23 +0600

    frontend ui all done alhamdulillah

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [currentState, setCurrentState] = useState("Sign Up");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800"
+    >
+      <div className="inline-flex items-center gap-2 mb-2 mt-10">
+        <p className="prata-regular text-3xl">{currentState}</p>
+        <hr className="border-none h-[1.5px] w-8 bg-gray-800" />
+      </div>
+      {/* input fields */}
+      {currentState === "Login" ? (
+        ""
+      ) : (
+        <input
+          required
+          type="text"
+          className="w-full px-3 py-2 border border-gray-800 outline-none"
+          placeholder="Name"
+        />
+      )}
+      <input
+        required
+        type="email"
+        className="w-full px-3 py-2 border border-gray-800 outline-none"
+        placeholder="Email"
+      />
+      <input
+        required
+        type="password"
+        className="w-full px-3 py-2 border border-gray-800 outline-none"
+        placeholder="Password"
+      />
+      <div className="w-full flex justify-between text-sm mt-[-8px]">
+        <p className="cursor-pointer hover:text-pink-500">
+          Forgot you password?
+        </p>
+        {currentState === "Login" ? (
+          <p
+            onClick={() => setCurrentState("Sign Up")}
+            className="cursor-pointer hover:text-pink-500"
+          >
+            Create account
+          </p>
+        ) : (
+          <p
+            onClick={() => setCurrentState("Login")}
+            className="cursor-pointer hover:text-pink-500"
+          >
+            Login here
+          </p>
+        )}
+      </div>
+      <button className="bg-black text-white font-light px-8 py-2 cursor-pointer hover:bg-transparent hover:text-black border hover:border-black transition-all">
+        {currentState === "Login" ? "Sign In" : "Sign Up"}
+      </button>
+    </form>
+  );
 };
 
 export default Login;
