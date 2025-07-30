f5d504a6cc398798a4c7cc8f5b2d98c2ae909301
Nafiuls <snafiul700@gmail.com>
2025-07-30 14:07:07 +0600

    now working on frontend with apis

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -88,7 +88,21 @@ const register = async (req, res) => {
 
 // route for admin login
 const adminLogin = async (req, res) => {
-  res.send("admin login");
+  try {
+    const { email, password } = req.body;
+    if (
+      email === process.env.ADMIN_EMAIL &&
+      password === process.env.ADMIN_PASSWORD
+    ) {
+      const token = jwt.sign(email + password, process.env.JWT_SECRET);
+      res.json({ success: true, token });
+    } else {
+      res.json({ success: false, message: "Invalid credentials" });
+    }
+  } catch (error) {
+    console.log("Error in adminLogin controller", error.message);
+    res.json({ success: false, message: error.message });
+  }
 };
 
 export { login, register, adminLogin };
