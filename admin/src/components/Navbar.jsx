b1dcce83a748c9fee81de13102e27015388a35bb
Nafiuls <snafiul700@gmail.com>
2025-07-31 13:18:14 +0600

    admin logout created

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { assets } from "../assets/assets";
 
-const Navbar = () => {
+const Navbar = ({ setToken }) => {
   return (
     <div className="flex items-center py-2 px-[4%] justify-between">
       <img className="w-[max(10%,80px)]" src={assets.logo} alt="" />
-      <button className="bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm cursor-pointer hover:bg-transparent border hover:border-gray-600 hover:text-gray-600 transition-all">
+      <button
+        onClick={() => setToken("")}
+        className="bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm cursor-pointer hover:bg-transparent border hover:border-gray-600 hover:text-gray-600 transition-all"
+      >
         Logout
       </button>
     </div>
